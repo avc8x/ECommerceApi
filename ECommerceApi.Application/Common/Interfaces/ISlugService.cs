012e05eeb4c93c09fabb7f88a9d2ece155c10852
Muhammad Khairallah <muha15kh@gmail.com>
2026-05-12 13:55:26 +0300

    Remove duplicate root folders

--- ECommerceApi.Application/Common/Interfaces/ISlugService.cs
+++
@@ -1,7 +0,0 @@
-namespace ECommerceApi.Application.Common.Interfaces;
-
-public interface ISlugService
-{
-    string GenerateSlug(string text);
-    Task<string> EnsureUniqueSlugAsync(string baseSlug, Guid? excludeId = null, CancellationToken cancellationToken = default);
-}
